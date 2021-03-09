a902994fc5c05cc54077c5d11da1748904ab63d9
mydatahack <thon6thon6@hotmail.com>
2021-03-10 09:37:12 +1100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12
+  parser: '@typescript-eslint/parser',
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12.
+    
   },
-  'rules': {
+  rules: {
     semi: ['error', 'always'],
     quotes: [2, 'single'],
     indent: ['error', 2]
